275117df3bd462dc4a4040e6dd14c264244252d0
shree-77 <shreedhoni7@gmail.com>
2023-11-11 22:33:06 +0530

    Dynamic todo updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
+import { TodoItem } from "./todo";
 
-export default function App(){
-  return ( 
-  <>
-  <form className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">
-        New Item
-      </label>
-      <input type="text" id="item"/>
-    </div>
-    <button className="btn">Add</button>
-  </form>
-  <h1 className="header">Todo List</h1>
-  <ul className="list">
-    <li>
-      <label htmlFor="">
-        <input type="checkbox"/>
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-     <li>
-      <label htmlFor="">
-        <input type="checkbox"/>
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-  </ul>
- </>
-  )
+export default function App() {
+  const [newItem, SetNewItem] = useState("");
+  const [todos, SetTodos] = useState([]);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    SetTodos(currentTodos =>{
+     return[
+       ...currentTodos,
+      {id:crypto.randomUUID(),title : newItem,
+      completed : false},
+     ]
+    });
+    SetNewItem("");
+  }
+  return (
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onInput={(e) => SetNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </>
+  );
 }
-
