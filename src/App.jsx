b32db795c704538aa35024a910550b6edeba788c
shree-77 <shreedhoni7@gmail.com>
2023-11-12 14:25:24 +0530

    Added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 
 export default function App(){
- const [todos,SetTodo] = useState([]);
+ const [todos,SetTodo] = useState(()=>{
+  const localValue = localStorage.getItem("ITEMS")
+  if(localValue==null) return[]
+  
+  return JSON.parse(localValue)
+ });
+
+ useEffect(()=>{
+  localStorage.setItem("ITEMS",JSON.stringify(todos))
+ },[todos])
 
   function addTodo(title){
      SetTodo(currentTodos =>{
